7cccc57daa0660154311f8ede5b29937e1349ea1
MATRIX <matrix@matrix-ytdemacbook-air.local>
2017-10-29 15:59:51 +0800

    Initial Commit

---
+++ Text_git/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Text_git
+//
+//  Created by MATRIX on 2017/10/29.
+//  Copyright © 2017年 MATRIX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Text_gitTests/Text_gitTests.m
@@ -0,0 +1,39 @@
+//
+//  Text_gitTests.m
+//  Text_gitTests
+//
+//  Created by MATRIX on 2017/10/29.
+//  Copyright © 2017年 MATRIX. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Text_gitTests : XCTestCase
+
+@end
+
+@implementation Text_gitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
