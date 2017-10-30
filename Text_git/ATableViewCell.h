1657a2662a7fe48a8931e89f6fdf6e8badb9229f
MATRIX <matrix@matrix-ytdemacbook-air.local>
2017-10-30 09:00:39 +0800

    create a atableview cell

---
+++ Text_git/ATableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ATableViewCell.h
+//  Text_git
+//
+//  Created by MATRIX on 2017/10/30.
+//  Copyright © 2017年 MATRIX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATableViewCell : UITableViewCell
+
+@end
---
+++ Text_git/ATableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  ATableViewCell.m
+//  Text_git
+//
+//  Created by MATRIX on 2017/10/30.
+//  Copyright © 2017年 MATRIX. All rights reserved.
+//
+
+#import "ATableViewCell.h"
+
+@implementation ATableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+    
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
